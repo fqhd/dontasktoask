13480c5e0c5d8587ac93b1cdafd14a5f6ca0d180
Fahd <fahd31256@gmail.com>
2023-06-19 12:51:01 +0100

    feat: robustness improvements

--- gen_training_data.py
+++ gen_training_data.py
@@ -1,134 +1,132 @@
+import tensorflow as tf
+from tensorflow import keras
 import numpy as np
 import random
 import string
-import pickle
 
-sequence_length = 50
-n_channels = 5
+def get_character_embeddings():
+    embeddings = []
+    for i in range(128):
+        curr = []
+        for j in range(7):
+            curr.append((i // pow(2, j)) % 2)
+        embeddings.append(curr)
+    return embeddings
 
-character_mappings = {
-    'a': [0, 0, 0, 0, 1],
-    'b': [0, 0, 0, 1, 0],
-    'c': [0, 0, 0, 1, 1],
-    'd': [0, 0, 1, 0, 0],
-    'e': [0, 0, 1, 0, 1],
-    'f': [0, 0, 1, 1, 0],
-    'g': [0, 0, 1, 1, 1],
-    'h': [0, 1, 0, 0, 0],
-    'i': [0, 1, 0, 0, 1],
-    'j': [0, 1, 0, 1, 0],
-    'k': [0, 1, 0, 1, 1],
-    'l': [0, 1, 1, 0, 0],
-    'm': [0, 1, 1, 0, 1],
-    'n': [0, 1, 1, 1, 0],
-    'o': [0, 1, 1, 1, 1],
-    'p': [1, 0, 0, 0, 0],
-    'q': [1, 0, 0, 0, 1],
-    'r': [1, 0, 0, 1, 0],
-    's': [1, 0, 0, 1, 1],
-    't': [1, 0, 1, 0, 0],
-    'u': [1, 0, 1, 0, 1],
-    'v': [1, 0, 1, 1, 0],
-    'w': [1, 0, 1, 1, 1],
-    'x': [1, 1, 0, 0, 0],
-    'y': [1, 1, 0, 0, 1],
-    'z': [1, 1, 0, 1, 0],
-    ' ': [1, 1, 0, 1, 1]
-}
+def get_character_mappings():
+    embeddings = get_character_embeddings()
+    ascii_chars = string.ascii_letters + string.digits + string.punctuation + ' '
+    char_mappings = {}
+    for i in range(len(ascii_chars)):
+        char_mappings[ascii_chars[i]] = embeddings[i]
+    return char_mappings
 
-with open('data/greetings.txt', 'r') as file:
-    greetings = file.read().split('\n')
-with open('data/verbs.txt', 'r') as file:
-    verbs = file.read().split('\n')
-with open('data/technologies.txt', 'r') as file:
-    technologies = file.read().split('\n')
-with open('data/subjects.txt', 'r') as file:
-    subjects = file.read().split('\n')
-with open('data/templates.txt', 'r', encoding='utf-8') as file:
-    templates = file.read().split('\n')
-    random.shuffle(templates)
-with open('data/messages.txt', 'r', encoding='utf-8') as file:
-    messages = file.read().split('\n')
-    random.shuffle(messages)
+class DataGenerator(keras.utils.Sequence):
+    def __init__(self, templates, messages, sequence_length=50, batch_size=32, shuffle=True):
+        self.sequence_length = sequence_length
+        self.batch_size = batch_size
+        self.batch_half_size = batch_size // 2
+        self.shuffle=shuffle
+        self.character_mappings = get_character_mappings()
+        with open('data/greetings.txt', 'r') as file:
+            self.greetings = file.read().split('\n')
+        with open('data/verbs.txt', 'r') as file:
+            self.verbs = file.read().split('\n')
+        with open('data/technologies.txt', 'r') as file:
+            self.technologies = file.read().split('\n')
+        with open('data/subjects.txt', 'r') as file:
+            self.subjects = file.read().split('\n')
+        with open(templates, 'r', encoding='utf-8') as file:
+            self.templates = file.read().split('\n')
+        with open(messages, 'r', encoding='utf-8') as file:
+            self.messages = file.read().split('\n')
+        self.on_epoch_end()
 
-def generate_message(train):
-    if train:
-        msg_template = random.choice(templates[:-10])
-    else:
-        msg_template = random.choice(templates[-10:])
-    tokens = msg_template.split(' ')
-    final = ''
-    tech = random.choice(technologies)
-    if random.randint(0, 2) == 0:
-        final += random.choice(greetings) + ' '
-    for t in tokens:
-        if t == '<verb>':
-            final += random.choice(verbs) + ' '
-        elif t == '<subject>':
-            final += random.choice(subjects) + ' '
-        elif t == '<technology>':
-            final += tech + ' '
-        else:
-            final += t + ' '
-    return final[:-1]
+    def generate_message(self):
+        msg_template = random.choice(self.templates)
+        tokens = msg_template.split(' ')
+        final = ''
+        if random.randint(0, 2) == 0:
+            final += random.choice(self.greetings)
+            should_add_comma = random.randint(0, 4)
+            if should_add_comma == 0:
+                final += ','
+            elif should_add_comma == 1:
+                final += ', '
+            elif should_add_comma == 2:
+                final += ' ,'
+            elif should_add_comma == 3:
+                final += ' , '
+            else:
+                final += ' '
+        for t in tokens:
+            if t == '<verb>':
+                final += random.choice(self.verbs) + ' '
+            elif t == '<subject>':
+                final += random.choice(self.subjects) + ' '
+            elif t == '<technology>':
+                final += random.choice(self.technologies) + ' '
+            else:
+                final += t + ' '
+        return final[:-1]
 
-def mutate_message(msg):
-    final = ''
-    for e in msg:
-        r = random.randint(0, 499) # 1/100 chance of mutation
-        if r == 0:
-            continue
-        elif r == 1:
-            final += e + e
-        elif r == 2:
-            final += random.choice(string.ascii_letters[:26]) + e
-        elif r == 3:
-            final += e + random.choice(string.ascii_letters[:26])
-        elif r == 4:
-            final += random.choice(string.ascii_letters[:26])
-        else:
-            final += e
-    return final
+    def on_epoch_end(self):
+        self.indexes = np.arange(len(self.messages))
+        if self.shuffle == True:
+            np.random.shuffle(self.indexes)
 
-def vectorize_message(msg):
-    vec = []
-    msg = msg.lower()
-    for c in msg:
-        if len(vec) == sequence_length * n_channels:
-            return vec
-        if c in character_mappings:
-            vec += character_mappings[c]
-    while len(vec) < sequence_length * n_channels:
-        vec += [0, 0, 0, 0, 0]
-    return vec
+    def mutate_message(self, msg):
+        final = ''
+        for e in msg:
+            r = random.randint(0, 199) # 1/75 chance of mutation
+            if r == 0:
+                continue
+            elif r == 1:
+                final += e + e
+            elif r == 2:
+                final += random.choice(string.ascii_letters[:26]) + e
+            elif r == 3:
+                final += e + random.choice(string.ascii_letters[:26])
+            elif r == 4:
+                final += random.choice(string.ascii_letters[:26])
+            else:
+                final += e
+        return final
 
+    def vectorize_message(self, msg):
+        vec = []
+        msg = msg.lower()
+        for c in msg:
+            if len(vec) == self.sequence_length * 7:
+                return vec
+            if self.character_mappings.get(c) is not None:
+                vec += self.character_mappings[c]
+        while len(vec) < self.sequence_length * 7:
+            vec += [1, 1, 1, 1, 1, 1, 1]
+        return vec
 
-train_x = []
-train_y = []
-test_x = []
-test_y = []
+    def __data_generation(self, indexes):
+        X = np.empty((self.batch_size, self.sequence_length * 7))
 
-# Make training data
-for msg in messages[:-500]:
-    train_x.append(vectorize_message(msg))
-    train_y.append(0)
+        zeros = np.zeros((self.batch_size // 2))
+        ones = np.ones((self.batch_size // 2))
 
-for i in range(len(messages) - 500):
-    train_x.append(vectorize_message(mutate_message(generate_message(train=True))))
-    train_y.append(1)
+        y = np.concatenate((zeros, ones))
+        np.random.shuffle(y)
 
-# Make testing data
+        itr_fmsg = iter(indexes)
+        for index, e in enumerate(y):
+            msg = self.mutate_message(self.generate_message()) if e else self.messages[next(itr_fmsg)]
+            X[index] = self.vectorize_message(msg)
 
-for msg in messages[-500:]:
-    test_x.append(vectorize_message(msg))
-    test_y.append(0)
+        return X, y
 
-for i in range(500):
-    test_x.append(vectorize_message(mutate_message(generate_message(train=False))))
-    test_y.append(1)
+    def __len__(self):
+        return int((len(self.messages) * 2 / self.batch_size))
 
-data = (np.array(train_x), np.array(train_y, dtype='int8')), (np.array(test_x), np.array(test_y, dtype='int8'))
+    def __getitem__(self, index):
+        indexes = self.indexes[index*self.batch_half_size:(index+1)*self.batch_half_size]
 
-with open('data/data.ds', 'wb') as f_out:
-    pickle.dump(data, f_out)
+        X, y = self.__data_generation(indexes)
 
+        return X, y
